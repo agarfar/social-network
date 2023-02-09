36eb6b873162406ced8666db66612713f2cd6d50
Soprano <antfar67@gmail.com>
2023-02-08 21:59:47 -0800

    replaced local host in connection with port

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/userThoughts', {
+connect('mongodb://127.0.0.1:27017/userThoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
