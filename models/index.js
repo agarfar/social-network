a64bfc89d4eef9a121492a192cdaea2665f6cebe
Soprano <antfar67@gmail.com>
2023-02-08 21:58:22 -0800

    Rewriting files to use correct routes/names

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const User = require('./User');
 const Reaction = require('./Reaction');
-const Thought = require('./Thoguht');
+const Thought = require('./Thought');
 
 module.exports = { User, Reaction, Thought };
